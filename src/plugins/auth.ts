00af9e61c47e7a774b1ac0db2616baf3c872e75d
Dragon <thomasdvandeweerd@gmail.com>
2023-01-05 06:00:10 +0100

    Added some simple authentication for now

---
+++ src/plugins/auth.ts
@@ -0,0 +1,41 @@
+import { FastifyPluginAsync } from 'fastify';
+import fp from 'fastify-plugin';
+import { decode } from '../util/jwt';
+import { toBigInt } from '../util/util';
+
+const plugin: FastifyPluginAsync = fp(async (instance, _options) => {
+    // Initialize the request with a null value, to be changed in the prehandler
+    instance.decorateRequest('user', null);
+
+    instance.addHook('preHandler', async (req, reply) => {
+        // No authorization found
+        if (req.headers.authorization === undefined) {
+            return;
+        }
+
+        /*
+            DEV TESTING
+            // TODO remove this
+        */
+        if (req.headers.authorization.startsWith('Development ')) {
+            const id = toBigInt(req.headers.authorization.substring(12));
+            req.user = { id };
+            return;
+        }
+
+        // Invalid token, we send back a 400 BAD REQUEST
+        if (!req.headers.authorization.startsWith('Bearer ')) {
+            return reply.status(400).send();
+        }
+
+        const jwt = req.headers.authorization.substring(7);
+
+        const payload = decode(jwt);
+
+        req.user = {
+            id: payload.id,
+        };
+    });
+});
+
+export default plugin;
--- src/types/fastify.d.ts
+++ src/types/fastify.d.ts
@@ -1,9 +1,12 @@
 declare global {
-    import { PrismaClient } from '@prisma/client';
+    import { PrismaClient, User } from '@prisma/client';
 
     module 'fastify' {
         interface FastifyInstance {
             prisma: PrismaClient;
+        }
+        interface FastifyRequest {
+            user: User
         }
     }
 }
