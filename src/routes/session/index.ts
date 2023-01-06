a8d908af1f5cd71810bd04dbae26b6db7bbc850a
Dragon <thomasdvandeweerd@gmail.com>
2023-01-06 04:54:41 +0100

    Added two more session endpoints

--- src/routes/session/index.ts
+++ src/routes/session/index.ts
@@ -1,5 +1,5 @@
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
-import { toBigInt } from '../../util/util';
+import { toBigInt } from '@/util/util';
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async function (
     instance,
@@ -41,6 +41,121 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async function (
             });
 
             return reply.status(200).send(sessions);
+        },
+    );
+
+    /*
+        Create session % HTTP POST /sessions
+    */
+    instance.post('/', {}, async (req, reply) => {
+        if (req.user === undefined) {
+            return reply.status(401).send();
+        }
+
+        const userSession = await instance.prisma.session.findFirst({
+            take: -1,
+            where: {
+                user: req.user.id,
+            },
+        });
+
+        // If the user has a previous session, make sure that it has ended
+        if (userSession !== null && userSession.original_end_time === null) {
+            return reply.status(409).send();
+        }
+
+        const id = instance.snowflake.generate();
+        const startTime = new Date();
+
+        const session = await instance.prisma.session.create({
+            data: {
+                id: id,
+                original_start_time: startTime,
+                start_time: startTime,
+                user: req.user.id,
+            },
+        });
+
+        return reply.status(201).send(session);
+    });
+
+    /*
+        Edit session % HTTP PATCH /sessions/{session.id}
+    */
+    instance.patch(
+        '/:id',
+        {
+            schema: {
+                params: {
+                    type: 'object',
+                    properties: {
+                        id: { type: 'string' },
+                    },
+                },
+                body: {
+                    type: 'object',
+                    properties: {
+                        description: { type: 'string' },
+                        start_time: { type: 'string', format: 'date-time' },
+                        end_time: { type: 'string', format: 'date-time' },
+                        original_end_time: {
+                            type: 'string',
+                            format: 'date-time',
+                        },
+                    },
+                },
+            } as const,
+        },
+        async (req, reply) => {
+            if (req.user === undefined) {
+                return reply.status(401).send();
+            }
+
+            // Get session by id and user id
+            const session = await instance.prisma.session.findFirst({
+                where: {
+                    id: toBigInt(req.params.id),
+                    user: req.user.id,
+                },
+            });
+
+            // If the user does not have a session with this id, we send back a 404 NOT FOUND
+            if (session === null) {
+                return reply.status(404).send();
+            }
+
+            // If the session has not been ended, and this request does not end it, we send back a 409 CONFLICT
+            if (
+                session.original_end_time === null &&
+                req.body.original_end_time === undefined
+            ) {
+                return reply.status(409).send();
+            }
+
+            // If the session has already ended, and the request tries to end it, we send back a 409 CONFLICT
+            if (
+                session.original_end_time !== null &&
+                req.body.original_end_time !== undefined
+            ) {
+                return reply.status(409).send();
+            }
+
+            // If the session has not ended yet, and no end_time was supplied, set it to the original_end_time
+            if (
+                session.original_end_time === null &&
+                req.body.end_time === undefined
+            ) {
+                req.body.end_time = req.body.original_end_time;
+            }
+            // Update the session in the database
+            const patchedSession = await instance.prisma.session.update({
+                where: {
+                    id: session.id,
+                },
+                data: req.body,
+            });
+
+            reply.status(200).send(patchedSession);
         },
     );
 };
