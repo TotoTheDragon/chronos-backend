c4360eb41977e381d77cb27979005d8ec6823c72
Dragon <thomasdvandeweerd@gmail.com>
2023-02-06 01:22:02 +0100

    Write prisma User model

---
+++ prisma/migrations/20230206001944_add_user/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - Added the required column `email` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `first_name` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `last_name` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "email" TEXT NOT NULL,
+ADD COLUMN     "first_name" TEXT NOT NULL,
+ADD COLUMN     "last_name" TEXT NOT NULL,
+ADD COLUMN     "password" TEXT NOT NULL,
+ADD COLUMN     "verification_token" TEXT,
+ADD COLUMN     "verified" BOOLEAN NOT NULL DEFAULT false;
